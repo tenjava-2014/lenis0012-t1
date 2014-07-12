772102b6bcb38f6e1c5894f99dd3faf5be76f86c
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-07-12 10:20:05 +0200

    lightning

--- src/main/java/com/tenjava/entries/lenis0012/t1/features/weapons/types/ThorsHammer.java
+++ src/main/java/com/tenjava/entries/lenis0012/t1/features/weapons/types/ThorsHammer.java
@@ -1,23 +1,23 @@
 package com.tenjava.entries.lenis0012.t1.features.weapons.types;
 
-import com.tenjava.entries.lenis0012.t1.Feature;
 import com.tenjava.entries.lenis0012.t1.TenJava;
-import com.tenjava.entries.lenis0012.t1.features.weapons.WeaponFeature;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.util.Vector;
+
+import java.util.Random;
 
 /**
  * Created by Lenny on 12-7-2014.
@@ -29,6 +29,9 @@ import org.bukkit.util.Vector;
 public class ThorsHammer extends BaseWeapon implements Listener {
     private final double speed;
     private final int radius;
+    private final int minLightning;
+    private final int maxLightning;
+    private final Random random = new Random();
 
     /**
      * @param config      Configuration of the weapon.
@@ -38,6 +41,8 @@ public class ThorsHammer extends BaseWeapon implements Listener {
 
         this.speed = config.getDouble("speed");
         this.radius = config.getInt("radius");
+        this.minLightning = config.getInt("min-lightning");
+        this.maxLightning = config.getInt("max-lightning") + 1;
         Bukkit.getPluginManager().registerEvents(this, TenJava.getInstance());
     }
 
@@ -62,9 +67,12 @@ public class ThorsHammer extends BaseWeapon implements Listener {
                         location = location.add(x, y, z);
                         Block block = location.getBlock();
                         Material type = block.getType();
-                        byte data = block.getData();
-                        block.setType(Material.AIR);
-                        FallingBlock falling = location.getWorld().spawnFallingBlock(location, type, data);
+                        if(type != Material.AIR) {
+                            byte data = block.getData();
+                            block.setType(Material.AIR);
+                            location.getWorld().spawnFallingBlock(location, type, data);
+                        }
+
                         location = location.subtract(x, y, z);
                     }
                 }
@@ -72,11 +80,29 @@ public class ThorsHammer extends BaseWeapon implements Listener {
 
             //Make explosion
             location.getWorld().createExplosion(location, radius);
+
+            //Make lightning
+            int count = minLightning + random.nextInt(maxLightning - minLightning);
+            for(int i = 0; i < count; i++) {
+                double dx = random.nextInt(radius * 2 + 1) - radius;
+                double dz= random.nextInt(radius * 2 + 1) - radius;
+                location = location.add(dx, 0, dz);
+                location.getWorld().strikeLightningEffect(location);
+                location = location.subtract(dx, 0, dz);
+            }
         }
     }
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
+        final Entity entity = event.getEntity();
+        if(entity != null && entity.hasMetadata("thorshammer")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
         final Entity entity = event.getEntity();
         if(entity.hasMetadata("thorshammer")) {
             event.setCancelled(true);
